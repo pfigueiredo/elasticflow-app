f8186b08af91e6c7e961264695f8772db735f427
Pedro Figueiredo <pedro@t3k.pt>
2022-10-30 17:13:45 +0000

    Implementation of new Curved wires

--- elasticflow-app/src/components/editor/Utils/LinkPath.js
+++ elasticflow-app/src/components/editor/Utils/LinkPath.js
@@ -31,8 +31,6 @@
      let tolerance = 16;
      const grids = 8;
  
-     const radius = 1; //props.roundCorner ? 1 : 0;
- 
      const stepX = distanceX / grids;
      const stepY = distanceY / grids;
  
@@ -41,99 +39,82 @@
      }
  
     let step = Math.min(Math.abs(stepX), Math.abs(stepY));
-         
-    function lineT1C() {
-        let path = `
-            M ${coordinates.start.x} ${coordinates.start.y}
-            c ${stem} 0 ${-stem} ${distanceY} ${distanceX} ${distanceY}
-        `;
-        return path;
-    }
+    //step = grids * Math.round(step / grids);
+    step = 8; //(Math.min(stepX, stepY) < 8) ? 0 : 8;
 
-    
-
-    function lineT1S() {
-
-        const factor = distanceX * distanceY > 0 ? 1 : -1;
-
+    function lineT1Q() {
+        const qsize = (distanceX - ((stem) * 2)) / 4;
+        
         let path = `
         M ${coordinates.start.x} ${coordinates.start.y} 
-        h ${stem + indexSpread} 
-        q ${step * radius} 0 ${step * 1 * radius} ${step * factor * radius}
-        V ${coordinates.end.y - step * factor * radius}
-        q 0 ${step * factor * radius} ${step * radius} ${step * factor * radius}
+        h ${stem} 
+        c ${qsize} 0, ${qsize} ${distanceY}, ${distanceX - (stem)} ${distanceY}
         H ${coordinates.end.x}
         `;
         return path;
     }
 
-    function lineT1() {
+    function lineT1S() {
+        const factor = distanceX * distanceY > 0 ? 1 : -1;
+
         let path = `
-            M ${coordinates.start.x} ${coordinates.start.y}
-            h ${stem + indexSpread}
-            v ${distanceY}
-            H ${coordinates.end.x}
+        M ${coordinates.start.x} ${coordinates.start.y} 
+        h ${stem + indexSpread} 
+        q ${step} 0 ${step} ${step * factor}
+        V ${coordinates.end.y - step * factor}
+        q 0 ${step * factor} ${step} ${step * factor}
+        H ${coordinates.end.x}
         `;
         return path;
     }
 
     function lineT2S() {
 
-        const factor = 1;
-
         const turnPoint = 
             (distanceY <= 40) ? 20 :
             (distanceY <= 48) ? 24 : 32;
+
         let path = `
             M ${coordinates.start.x} ${coordinates.start.y}
             h ${stem + indexSpread}
-            q ${step * radius} 0 ${step * 1 * radius} ${step * radius}
-            v ${distanceY - turnPoint}
-            q ${step * -radius} 0 ${-step * 1 * -radius} ${-step * radius}
+            q ${step} 0 ${step} ${step}
+            v ${distanceY - turnPoint - (step * 2)}
+            q 0 ${step} ${-step} ${step}
             h ${distanceX - (stem * 2)}
-            v ${turnPoint}
+            q ${-step} 0 ${-step} ${step}
+            v ${turnPoint - (step * 2)}
+            q 0 ${step} ${step} ${step}
             H ${coordinates.end.x}
         `;
         return path;
     }
 
-
-    function lineT2() {
-        const turnPoint = 
-            (distanceY <= 40) ? 20 :
-            (distanceY <= 48) ? 24 : 32;
+    function lineT3S() {
         let path = `
             M ${coordinates.start.x} ${coordinates.start.y}
             h ${stem + indexSpread}
-            v ${distanceY - turnPoint}
-            h ${distanceX - (stem * 2)}
-            v ${turnPoint}
+            q ${step} 0 ${step} ${step}
+            v ${24 - (step * 2)}
+            q 0 ${step} ${-step} ${step}
+            h ${distanceX - (stem * 2 + step)}
+            q ${-step} 0 ${-step} ${-step}
+            v ${distanceY -(24 - (step * 2))}
+            q 0 ${-step} ${step} ${-step}
             H ${coordinates.end.x}
         `;
         return path;
-    }
+    }    
 
-    function lineT3() {
-        let path = `
-            M ${coordinates.start.x} ${coordinates.start.y}
-            h ${stem + indexSpread}
-            v ${24}
-            h ${distanceX - (stem * 2)}
-            v ${distanceY -24}
-            H ${coordinates.end.x}
-        `;
-        return path;
-    }
-
-    //  return corner34();
-    
-
-     if (distanceX > (stem + tolerance)) {
-        return lineT1S();
+    if (distanceX > (stem + tolerance)) {
+        if (Math.abs(distanceY) <= 8)
+            return lineT1Q();
+        else 
+            return lineT1S();
      } else if (distanceY > 16) {
         return lineT2S();
      } else {
-        return lineT3();
+        //TODO: if -distanceX < size of activity we need a new S curve similar to T2S but inverted
+        return lineT3S();
      }
  
  }
